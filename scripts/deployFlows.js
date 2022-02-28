564bc107b769df1c149aaeb475768ac4adb9044b
omar montoya <montoyaomar01@hotmail.com>
2022-02-28 17:26:09 -0500

    chore: modify actions for multiple files

--- scripts/deployFlows.js
+++ scripts/deployFlows.js
@@ -6,50 +6,62 @@ const { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN } = process.env
 const client = require('twilio')(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
 async function run() {
-  const filePath = path.resolve(__dirname, '../flows/BGT_Flow.json');
-  const flow = JSON.parse(await fs.readFile(filePath, 'utf8'));
-  const friendlyName = 'BGT_Flow';
+  const folderPath = path.resolve(__dirname, '../flows/');
+  let files = await fs.readdir(folderPath, 'utf8')
+  files = files.filter ( file => {
+    return path.extname(file) === ".json"
+  })
 
-  try {
-    await client.studio.flowValidate.update({
+  files.map( async (file) => {
+    const filePath = `${folderPath}/${file}`
+    const flow = JSON.parse(await fs.readFile(filePath, 'utf8'));
+    const friendlyName = file.split('.')[0];
+
+    const date = new Date().toLocaleString('es-CO')
+    const commitMessage = `Changes: ${date}`
+
+    try {
+      await client.studio.flowValidate.update({
+        definition: flow,
+        friendlyName,
+        status: 'published',
+      });
+    } catch (err) {
+      console.error('Invalid Flow: ', friendlyName);
+      console.dir(err.details);
+      return;
+    }
+
+    const allFlows = await client.studio.flows.list();
+    const existingFlow = allFlows.find(
+      (flow) => flow.friendlyName === friendlyName
+    );
+
+    if (!existingFlow) {
+      const newFlow = await client.studio.flows.create({
+        definition: flow,
+        friendlyName,
+        commitMessage,
+        status: 'published',
+      });
+      console.log('New Flow: ', newFlow.friendlyName);
+      return;
+    }
+
+    const { definition } = await client.studio.flows(existingFlow.sid).fetch();
+    if (isEqual(definition, flow)) {
+      console.log('No changes: ', existingFlow.friendlyName );
+      return;
+    }
+
+    const updatedFlow = await client.studio.flows(existingFlow.sid).update({
       definition: flow,
-      friendlyName,
       status: 'published',
+      commitMessage,
     });
-  } catch (err) {
-    console.error('Invalid Flow');
-    console.dir(err.details);
-    return;
-  }
+    console.log('Updated flow: ', updatedFlow.friendlyName);
 
-  const allFlows = await client.studio.flows.list();
-  const existingFlow = allFlows.find(
-    (flow) => flow.friendlyName === friendlyName
-  );
-
-  if (!existingFlow) {
-    const newFlow = await client.studio.flows.create({
-      definition: flow,
-      friendlyName,
-      commitMessage: "first flow",
-      status: 'published',
-    });
-    console.log('New Flow', newFlow.webhookUrl);
-    return;
-  }
-
-  const { definition } = await client.studio.flows(existingFlow.sid).fetch();
-  if (isEqual(definition, flow)) {
-    console.log('No changes');
-    return;
-  }
-
-  const updatedFlow = await client.studio.flows(existingFlow.sid).update({
-    definition: flow,
-    status: 'published',
-    commitMessage: 'Automated deployment',
-  });
-  console.log('Updated flow', updatedFlow.webhookUrl);
+  })
 }
 
 run().catch(console.error);
